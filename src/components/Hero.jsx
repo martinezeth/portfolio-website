351a1f8721c32be9b1cfe9f044fed1e0518eff89
martinezeth <emartinez34@att.net>
2024-09-25 18:11:06 -0700

    Fixed routing for animated icon in Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
 import { styles } from '../styles';
-import { navLinks } from '../constants';
 import { abstractspiderweb } from '../assets';
 
 const Hero = () => {
+  const handleScrollToAbout = () => {
+    const aboutSection = document.getElementById('about');
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <section className="relative w-full h-[60vh] mx-auto overflow-hidden">
       <div className="absolute top-0 left-0 z-0 w-full h-full">
@@ -42,23 +47,23 @@ const Hero = () => {
 
       <div className="absolute xs:bottom-10 bottom-32 w-full 
         flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 
+        <div
+          onClick={handleScrollToAbout}
+          className="w-[35px] h-[64px] rounded-3xl border-4 
             border-french border-dim flex
-            justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: 'loop',
-              }}
-              className="w-3 h-3 rounded-full bg-taupe mb-1"
-            />
-          </div>
-        </a>
+            justify-center items-start p-2 cursor-pointer">
+          <motion.div
+            animate={{
+              y: [0, 24, 0],
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop',
+            }}
+            className="w-3 h-3 rounded-full bg-taupe mb-1"
+          />
+        </div>
       </div>
 
       {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
